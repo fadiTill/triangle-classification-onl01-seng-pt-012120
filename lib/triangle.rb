fef3bea89c63c5f1ad5cb7b741c0d2fdcb892536
fadimata tillman <fadi.tillman@gmail.com>
2020-02-25 19:30:41 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,45 @@
 class Triangle
-  # write code here
+
+ attr_accessor :lenght1, :length2, :lenght3, :kind
+
+  def initialize (lenght1,length2, lenght3)
+    @lenght1 = lenght1
+    @length2 = length2
+    @lenght3 = lenght3
+   # write code here
+ end
+#
+#
+ def kind
+
+   if (lenght1 * length2 *l enght3 == 0)|| (lenght1 + length2) <=l enght3| |(length2 + lenght3) <= lenght1 || (lenght3 +l enght1 )<=length2
+begin
+  raise TriangleError
+  puts error.message
 end
+
+elsif lenght1 == length2 && lenght1 == lenght3
+  self.kind = :equilateral
+elsif lenght3 == length2 || lenght1 == lenght3 ||length2 == lenght1
+  self.kind = :isosceles
+else
+  self.kind = :scalene
+end
+end
+end
+
+
+ #  if @lenght1 = @length2 && = @lenght3
+ #    puts "equilateral"
+ #  elsif @lenght1 != @length2 = @lenght3
+ #     puts "isosceles"
+ #  else @lenght1!= @length2 != @lenght3
+ #     puts "scalene"
+ # end
+ # end
+ # end
+ class TriangleError < StandardError
+   def message
+     puts " not a triangle"
+ end
+ end
